03e4c92243396a4c9895434dc467518120f82a97
monkbysea <TAOWEI@TAOs-MBP.wireless.newschool.edu>
2016-09-12 16:52:46 -0400

    javascript updated

--- homework/w01_h01_javascript/JS/script.js
+++ homework/w01_h01_javascript/JS/script.js
@@ -1,33 +1,4 @@
-//FIRST TRY
-// document.getElementById("about").onclick = function() {onClick()};
-// // document.getElementById("about").onmouseout = function() {mouseOut()};
 
-// function onClick() {
-//     document.getElementById("sub-about").style.display = "block";
-    
-// function mouseOut() {
-//     document.getElementById("sub-about").style.display= "none";
-// }
-
-
-//SECOND TRY
-// var item = document.getElementById("about");
-// item.addEventListener("mouseover", func, false);
-// item.addEventListener("mouseout", func1, false);
-
-// function func()
-// {  // not needed since item is already global, 
-//    // I am assuming this is here just because it's sample code?
-//    // var item = document.getElementById("button"); 
-//    item.setAttribute("style", "background-color:blue;")
-// }
-
-// function func1()
-// {  
-//    item.setAttribute("style", "background-color:green;")
-// }
-
-//THIRD TRY
 document.getElementById('about').onmouseover= function flyout1(){
 	document.getElementById('sub-about').classList.add("show");
 }
@@ -54,8 +25,3 @@ document.getElementById('contact').onmouseout= function flyin3(){
 
 
 
-// FOURTH TRY
-// document.getElementById('about').onclick= function(){flyout()};
-// function flyout(){
-// 	document.getElementById('sub-about').style.visibility='show';
-// }
