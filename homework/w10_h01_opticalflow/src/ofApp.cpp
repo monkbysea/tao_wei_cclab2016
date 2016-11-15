c653103b4635d7ce753bb65ab296174d8985ec01
monkbysea <TAOWEI@TAOs-MBP.wireless.newschool.edu>
2016-11-14 19:56:38 -0500

    homework_w10

---
+++ homework/w10_h01_opticalflow/src/ofApp.cpp
@@ -0,0 +1,127 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    grabber.setup(ofGetWidth(), ofGetHeight());
+    flowSolver.setup(grabber.getWidth(), grabber.getHeight(), 0.35, 5, 10, 1, 3, 2.25, false, false);
+    ofSetBackgroundAuto(false);
+    
+    for (int i = 0; i < 2000; i++) {
+        particle p = particle();
+        p.pos.set(ofRandom(0, ofGetWidth()), ofRandom(0, ofGetHeight()));
+        p.vel.set(0, 0);
+        particles.push_back(p);
+//        lines.push_back(ofPolyline());
+//        lineWidths.push_back(ofRandom(1, 10));
+    }
+//    ofSetFrameRate(60);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    grabber.update();
+    grabber.getPixels().mirror(false, true);
+    if (grabber.isFrameNew()) {
+        flowSolver.update(grabber);
+        
+        for (int i = 0; i < particles.size(); i++) {
+            particles[i].resetForce();
+            particles[i].addForce(0.0, 0.1);
+            ofPoint flowVelocity = flowSolver.getVelAtPixel(particles[i].pos.x, particles[i].pos.y) * 0.1;
+            particles[i].addForce(flowVelocity.x, flowVelocity.y);
+            particles[i].addDampingForce();
+            particles[i].update();
+            particles[i].wrapAroundWalls();
+//            lines[i].addVertex(particles[i].pos);
+//            
+//            if (lines[i].size() > 2) {
+//                lines[i].getVertices().erase(lines[i].getVertices().begin());
+//            }
+        }
+    }
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofPixels pixels = grabber.getPixels();
+    ofRectangle viewport = ofGetCurrentViewport();
+    for (int i = 0; i < particles.size(); i++) {
+        if (viewport.inside(particles[i].pos)) {
+            ofSetColor(pixels.getColor(particles[i].pos.x, particles[i].pos.y));
+                        particles[i].draw();
+            //            ofSetLineWidth(lineWidths[i]);
+            //            lines[i].draw();
+//            if (lines[i].getVertices().size() > 0) {
+//                drawLineAsRect(lines[i].getVertices().front(), lines[i].getVertices().back(), lineWidths[i]);
+//            }
+        }
+    }
+
+}
+
+//void ofApp::drawLineAsRect(ofPoint startVec, ofPoint endVec, float thickness) {
+//    float angle = atan((endVec.y-startVec.y)/(endVec.x-startVec.x));
+//    ofPushMatrix();
+//    ofTranslate(startVec.x,startVec.y);
+//    ofRotate(ofRadToDeg(angle));
+//    float lineLength = (endVec.x - startVec.x)/cos(angle);
+//    ofDrawRectangle(0, -thickness/2, lineLength, thickness);
+//    ofPopMatrix();
+//}
+
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
